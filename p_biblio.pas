68c65441fcaf01deb8fb997897c40cf76d263d38
Thibault Libert <thibault.libert@he-arc.ch>
2018-12-17 09:17:36 +0100

    Test

--- p_biblio.pas
+++ p_biblio.pas
@@ -6,11 +6,9 @@ USES u_biblio, u_livre, u_adherent, crt;
 		u_livre.initUnite();
 	END;
 	
-	//Procedure chargeant déjà quelques données de base
+	//Procedure chargeant dï¿½jï¿½ quelques donnï¿½es de base
 	PROCEDURE chargeDonneesInitiales(var biblio:Tbibliotheque; var adherent: Tadherent; var livre: Tlivre);
-	BEGIN
-
-	
+	BEGIN	
 		biblio.nomBiblio:='Arc Biblio';
 		biblio.adresse.rue:='Espace de l''Europe';
 		biblio.adresse.numeroRue:='21';
@@ -19,7 +17,7 @@ USES u_biblio, u_livre, u_adherent, crt;
 		biblio.adresse.pays:='Suisse';
 	
 	
-		//TODO : à compléter avec quelques adhérents et quelques livres
+		//TODO : ï¿½ complï¿½ter avec quelques adhï¿½rents et quelques livres
 		
 	END;
 
